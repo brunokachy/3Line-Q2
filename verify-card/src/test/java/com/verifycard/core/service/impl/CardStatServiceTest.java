a59ec0c0ae0db877b62071a97f5dd0c86038b940
Bruno <bruno.okafor@gmail.com>
2021-04-11 04:34:46 +0100

    Code in progress

---
+++ verify-card/src/test/java/com/verifycard/core/service/impl/CardStatServiceTest.java
@@ -0,0 +1,23 @@
+package com.verifycard.core.service.impl;
+
+import com.verifycard.core.service.BankCardPersistenceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class CardStatServiceTest {
+
+    @InjectMocks
+    private CardStatServiceImpl cardStatService;
+
+    @Mock
+    private BankCardPersistenceService bankCardPersistenceService;
+
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+
+
+}
---
+++ verify-card/src/test/java/com/verifycard/core/service/impl/CardVerificationServiceTest.java
@@ -0,0 +1,7 @@
+package com.verifycard.core.service.impl;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+public class CardVerificationServiceTest {
+}
